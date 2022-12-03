558dcbdbf3c1f64ac63215b94f8641d121fd3306
Eduard Kopylov <eduard.kopylov@qoollo.com>
2022-12-03 14:05:11 +0300

    feature/8 impovements

--- musify/src/components/SongItem.vue
+++ musify/src/components/SongItem.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-list-item :title="song.title" :subtitle="song.artist.name">
-    <template v-slot:prepend>
-      <v-avatar :image="song.artist.picture" :rounded="0"></v-avatar>
+  <v-hover open-delay="100" close-delay="00">
+    <template v-slot="{ isHovering, props }">
+      <v-list-item
+        v-bind="props"
+        class="song"
+        :title="song.title"
+        :subtitle="song.artist.name"
+        :style="
+          isHovering
+            ? { backgroundColor: '#e2e2e2' }
+            : { backgroundColor: '#FFF' }
+        "
+      >
+        <template v-slot:prepend>
+          <v-avatar
+            size="60"
+            :image="song.artist.picture"
+            class="rounded"
+          ></v-avatar>
+        </template>
+        <template v-slot:append>
+          <v-btn :icon="'mdi-play-outline'" variant="text"></v-btn>
+          <v-btn :icon="'mdi-menu'" variant="text"></v-btn>
+        </template>
+      </v-list-item>
     </template>
-    <template v-slot:append>
-      <v-btn :icon="'mdi-play-outline'" variant="text"></v-btn>
-      <v-btn :icon="'mdi-menu'" variant="text"></v-btn>
-    </template>
-  </v-list-item>
+  </v-hover>
 </template>
 
 <script>
