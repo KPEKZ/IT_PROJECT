89b64031c004895f2b81aac41822553f65b39e5e
Eduard Kopylov <eduard.kopylov@qoollo.com>
2022-12-12 20:42:30 +0300

    feature/17_album_fix

--- musify/src/views/AlbumView.vue
+++ musify/src/views/AlbumView.vue
@@ -1,23 +1,26 @@
 <template>
   <v-container class="container">
-    <v-card max-width="300">
-      <v-img
-        :src="this.songs[0]?.album.cover_big"
-        height="300px"
-        cover
-        class="align-end text-white"
-      >
-        <v-card-title class="title_text" v-text="this.songs[0]?.album.title">
-        </v-card-title>
-        <v-card-title
-          class="title_text"
-          v-text="this.songs[0]?.artist.name + ', tracks: ' + this.songs.length"
+    <v-row class="row justify-center">
+      <v-card max-width="300">
+        <v-img
+          :src="songs[0]?.album.cover_big"
+          height="300px"
+          width="300px"
+          cover
+          class="align-end text-white"
         >
-        </v-card-title>
-      </v-img>
-    </v-card>
+          <v-card-title class="title_text" v-text="songs[0]?.album.title">
+          </v-card-title>
+          <v-card-title
+            class="title_text"
+            v-text="songs[0]?.artist.name + ', tracks: ' + songs.length"
+          >
+          </v-card-title>
+        </v-img>
+      </v-card>
+    </v-row>
     <v-row> <h3 class="header">Songs</h3></v-row>
-    <v-row class="row rounded-lg">
+    <v-row class="row row__theme_default rounded-lg">
       <section class="songs">
         <SongsList
           v-if="songs"
@@ -47,7 +50,7 @@ export default {
       getAllSongsInAlbum(this.$route.params.id).then((res) => {
         const artistPicture = res.artist?.picture;
         const tracks = res?.tracks?.data;
-        this.songs = tracks.map((track) => {
+        this.songs = tracks?.map((track) => {
           const newTrack = { ...track };
           track.artist.picture = artistPicture;
           return newTrack;
@@ -67,9 +70,13 @@ export default {
 </script>
 <style scoped>
 .row {
-  box-shadow: 0 0 3px 1px rgba(34, 60, 80, 0.2);
   margin: 0;
 }
+
+.row__theme_default {
+  box-shadow: 0 0 3px 1px rgba(34, 60, 80, 0.2);
+}
+
 .header {
   color: #69686f;
   margin-left: 70px;
