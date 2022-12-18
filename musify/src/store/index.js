8f314e4bb919d0d7983b967fff3db5d65f47995e
Cachap <maksim.dorofeev.95@mail.ru>
2022-12-18 14:07:56 +0300

    Album in library

--- musify/src/store/index.js
+++ musify/src/store/index.js
@@ -98,14 +98,22 @@ export default createStore({
     },
     setLibraryAlbums(state, albums) {
       if (!Array.isArray(albums)) return;
-      state.libraryAlbums = albums;
+      state.libraryAlbums = albums.map((album) => {
+        const appId = Symbol.for("appId");
+        return { ...album, [appId]: Math.random() };
+      });
     },
     addLibraryAlbum(state, album) {
-      state.librarySongs.push(album);
+      const appId = Symbol.for("appId");
+      state.libraryAlbums.push({ ...album, [appId]: Math.random() });
     },
     addLibraryAlbums(state, albums) {
       if (!Array.isArray(albums)) return;
-      state.librarySongs.push(...albums);
+      const newAlbums = albums.map((album) => {
+        const appId = Symbol.for("appId");
+        return { ...album, [appId]: Math.random() };
+      });
+      state.libraryAlbums.push(...newAlbums);
     },
     addLibrarySongs(state, songs) {
       if (!Array.isArray(songs)) return;
@@ -162,6 +170,13 @@ export default createStore({
       const appId = Symbol.for("appId");
       state.librarySongs = oldLibrarySongs.filter(
         (s) => s[appId] !== song[appId]
+      );
+    },
+    deleteAlbumFromLibrary(state, album) {
+      const oldLibraryAlbums = [...state.libraryAlbums];
+      const appId = Symbol.for("appId");
+      state.libraryAlbums = oldLibraryAlbums.filter(
+        (s) => s[appId] !== album[appId]
       );
     },
     setCurrentSong(state, song) {
@@ -264,6 +279,9 @@ export default createStore({
     },
     deleteFromLibrarySong({ commit }, song) {
       commit("deleteSongFromLibrary", song);
+    },
+    deleteFromLibraryAlbum({ commit }, album) {
+      commit("deleteAlbumFromLibrary", album);
     },
     addLibraryAlbums({ commit }, albums) {
       commit("addLibraryAlbums", albums);
